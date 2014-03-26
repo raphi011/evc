e2dc67a6248af3b71c315b7cbffabd1a70af078f
Raphael Gruber <raphi011@gmail.com>
2014-03-26 20:13:36 +0100

    implemented methods

--- 1/MatlabBasics.m
+++ 1/MatlabBasics.m
@@ -22,6 +22,7 @@ function[v1, v2, v3, M, M_30x50, v_M_Product, M_v_Product, M_M_Product, M_M_comp
 %      A     B     C
 %      D     E     F
 %      G     A     B
+clc
 
 v1 = [0,8,2];
 v2 = [8;6;3];
@@ -43,7 +44,7 @@ v3 = 0:0.25:8;
 % information type:
 % help zeros
 
-M_30x50 = zeros(30,50)
+M_30x50 = zeros(30,50);
 
 %% II. implement your own versions of the following built-in Matlab functions:
 % *, .*, cross, dot
@@ -57,10 +58,10 @@ M_30x50 = zeros(30,50)
 % Hint: In order to check your solution, you can compare your results with
 % the results you get by using the built-in Matlab functions.
 
-% 1) Vector-Matrix-Multiplication (use v1 and M)
+% 1) Vector-Matrix-Multiplication (use v1 and M) -->  zeilenvektor!
 v_M_Product = vector_X_Matrix(v1, M);
 
-% 2) Matrix-Vector-Multiplication (use M and v2)
+% 2) Matrix-Vector-Multiplication (use M and v2) --> spaltenvektor!
 M_v_Product = Matrix_X_vector(M, v2);
 
 % 3) Matrix-Matrix-Multiplication 
@@ -73,69 +74,96 @@ M_M_Product = Matrix_X_Matrix(M, M2);
 M_M_componentwise = Matrix_Xc_Matrix(M, M2);
 
 % 5) cross product of 2 vectors (use v1 and v2)
+cross(v1,v2)
 crossP = crossProduct(v1,v2);
 
 % 6) dot product (use v1 and v2)
+dot(v1,v2)
 dotP = dotProduct(v1,v2);
 
 
 end
 
+
+
 function[result] = vector_X_Matrix(v1, M)
 
-% return a row vector
+matrixSize = size(M);
+vector = zeros(1,matrixSize(1));
 
-% TODO: your code goes here
-result = 0; % TODO: edit this
+for column=1:matrixSize(2)
+    for row=1:matrixSize(1)
+        vector(column) = vector(column) + (v1(row)*M(row,column));
+    end
+end
+
+result = vector;
 
 end
 
 function[result] = Matrix_X_vector(M, v2)
 
-%return a column vector
+matrixSize = size(M);
+vector = zeros(matrixSize(2),1);
 
-% TODO: your code goes here
-result = 0; % TODO: edit this
+for row=1:matrixSize(1)
+    for column=1:matrixSize(2)
+        vector(row) = vector(row) + (v2(column)*M(row,column));
+    end
+end
+
+result = vector;
 
 end
 
 function[result] = Matrix_X_Matrix(M, M2)
+newMatrix = zeros(size(M,1),size(M2,2));
 
-% hint: row times column!
+for i=1:size(M,1)
+    for j=1:size(M2,2)
+        for k=1:size(M,2)
+            newMatrix(j,i) = newMatrix(j,i) + (M(i,k)*M2(k,j));
+        end
+    end
+end
 
-% TODO: your code goes here
-result = 0; % TODO: edit this
+result = newMatrix
 
 end
 
 function[result] = Matrix_Xc_Matrix(M, M2)
+newMatrix = zeros(size(M,1), size(M,2));
 
-% implement component wise multiplication
+for i=1:size(M,1)
+    for j=1:size(M,2)
+        newMatrix(i,j) = M(i,j) * M2(i,j);
+    end
+end
 
-% TODO: your code goes here
-result = 0; % TODO: edit this
+result = newMatrix;
 
 end
 
 function[result] = crossProduct(v1,v2)
 
-% IMPORTANT: DON'T use the built-in Matlab function 'cross' to do this!!!
-% implement it yourself and then compare it with 'cross'
+rowVector = zeros(1,size(v1,1));
 
-% return a row vector
+rowVector(1) = v1(2)*v2(3) - v1(3)*v2(2);
+rowVector(2) = v1(3)*v2(1) - v1(1)*v2(3);
+rowVector(3) = v1(1)*v2(2) - v1(2)*v2(1);
 
-% TODO: your code goes here
-result = 0; % TODO: edit this
+result = rowVector
 
 end
 
 function[result] = dotProduct(v1,v2)
+dotproduct = 0;
 
-% IMPORTANT: DON'T use the built in Matlab function 'dot' to do this!!!
-% implement it yourself and then compare it with 'dot'
+for i=1:length(v1)
+    dotproduct =  dotproduct + v1(i)*v2(i);
+end
 
-% TODO: your code goes here
-result = 0; % TODO: edit this
+result = dotproduct
 
 end
 
